1720b76a5746887ee664b4af732acbcf2b97734d
KasididWa <kasididw@mottoauction.com>
2021-01-22 15:33:36 +0700

    Initial Commit

---
+++ neversitup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neversitup
+//
+//  Created by Kasidid Wachirachai on 22/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
